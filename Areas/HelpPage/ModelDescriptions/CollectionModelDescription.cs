dba5c7327c8979654da648f296ea1a3b1b09633d
Suraphel Mekasha <suraphel.mekasha@rokea.no>
2022-03-23 12:33:49 +0100

    my first commit

---
+++ Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BALEMOJA_API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BALEMOJA_API.WebApiApplication" Language="C#" %>
